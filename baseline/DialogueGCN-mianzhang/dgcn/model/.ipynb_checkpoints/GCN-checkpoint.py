e5ad8074da755f4e11d57d14d7958436bf5d9315
kakaeriol <nguyenpham2101@gmail.com>
2023-03-22 01:39:40 +0800

    Change to  DGL - original: 0.5796, miazang: 0.5718

--- baseline/DialogueGCN-mianzhang/dgcn/model/.ipynb_checkpoints/GCN-checkpoint.py
+++ baseline/DialogueGCN-mianzhang/dgcn/model/.ipynb_checkpoints/GCN-checkpoint.py
@@ -1,8 +1,9 @@
 import torch.nn as nn
-# from dgl.nn.pytorch import RelGraphConv as RGCNConv
-# from dgl.nn.pytorch import GraphConv
-from torch_geometric.nn import GraphConv
-from .RGCN import RGCNConv
+import dgl
+from dgl.nn.pytorch import RelGraphConv as RGCNConv
+from dgl.nn.pytorch import GraphConv
+# from torch_geometric.nn import GraphConv
+# from .RGCN import RGCNConv
 
 class GCN(nn.Module):
 
@@ -11,10 +12,15 @@ class GCN(nn.Module):
         self.num_relations = 2 * args.n_speakers ** 2
         self.conv1 = RGCNConv(g_dim, h1_dim, self.num_relations, num_bases=30)
         self.conv2 = GraphConv(h1_dim, h2_dim)
+        if args.device != 'cpu':
+            self.conv1 = self.conv1.cuda()
+            self.conv2 = self.conv2.cuda()
 
     def forward(self, node_features, edge_index, edge_norm, edge_type):
-        x = self.conv1(node_features, edge_index, edge_type, edge_norm=edge_norm)
-        x = self.conv2(x, edge_index)
+        g = dgl.graph((edge_index[0], edge_index[1]))
+        g.norm = edge_norm
+        x = self.conv1(g, node_features, edge_type)
+        x = self.conv2(g, x)
 
         return x
 
