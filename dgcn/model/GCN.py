4c538b3a9bde0fead9c097a04c3e4a21ac0c8a98
Mia <mhnguyen@u.nus.edu>
2023-03-21 16:27:25 +0800

    workable version, F1 = 0.58

---
+++ dgcn/model/GCN.py
@@ -0,0 +1,47 @@
+import torch
+import torch.nn as nn
+from torch_geometric.nn import RGCNConv, GraphConv
+# from .myRGCNConv import myRGCNConv
+import dgcn
+log = dgcn.utils.get_logger()
+
+class GCN(nn.Module):
+
+    def __init__(self, g_dim, h1_dim, h2_dim, args):
+        super(GCN, self).__init__()
+        self.num_relations = 2 * args.n_speakers ** 2
+        self.conv1 = RGCNConv(g_dim, h1_dim, self.num_relations, num_bases=30)
+        # self.conv1 = myRGCNConv(g_dim, h1_dim, self.num_relations, num_bases=30)
+        self.conv2 = GraphConv(h1_dim, h2_dim)
+
+    def forward(self, node_features, edge_index, edge_norm, edge_type):
+        x = self.conv1(node_features, edge_index, edge_type)
+        # x = self.conv1(node_features, edge_index, edge_type, edge_norm=edge_norm)
+        # x = self.conv2(x, edge_index, edge_weight=edge_norm)
+        # log.info("x.shape = {}, {}".format(x.shape, edge_norm.view(-1, 1).shape) )
+        x = self.conv2(x, edge_index, edge_weight=edge_norm)
+
+
+        return x
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
