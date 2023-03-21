6ee46064a3fa2d8f45b1c388b521c5da75832dcb
kakaeriol <nguyenpham2101@gmail.com>
2023-03-21 18:04:48 +0800

    base-line

---
+++ baseline/DialogueGCN/.ipynb_checkpoints/run_train-checkpoint.sh
@@ -0,0 +1,6 @@
+# python3 -u train_IEMOCAP.py \
+#       --base-model=GRU  --epochs=50 --dropout=0.5 \
+#      --batch-size=32 --lr=0.0003 > log_train_IEMOCAP
+
+python3 -u train_IEMOCAP.py \
+      --base-model=GRU > log_train_IEMOCAP
