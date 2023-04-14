729d946038126df29b9f6b527c56a4bdb790b583
Huong Nguyen <huongntm@comp.nus.edu.sg>
2023-04-14 18:36:22 +0800

    fixed bug

--- base_new/dgcn/model/DialogueGCN.py
+++ base_new/dgcn/model/DialogueGCN.py
@@ -9,6 +9,7 @@ from .functions import batch_graphify
 from .CNN import CNNFeatureExtractor
 import dgcn
 import pandas as pd
+import pickle
 log = dgcn.utils.get_logger()
 
 
@@ -16,15 +17,16 @@ class DialogueGCN(nn.Module):
 
     def __init__(self, args):
         super(DialogueGCN, self).__init__()
-        voca_size = 250
-        e_dim = 300
+        self.vocab_size = -1
+        self.e_dim = 300
         u_dim = 100
         g_dim = 200
         h1_dim = 100
         h2_dim = 100
         hc_dim = 100
         tag_size = 6
-        
+        if ("Daily" in args.data) or ("MELD" in args.data):
+            tag_size = 7
         cnn_filters=50
         cnn_kernel_sizes=(3, 4, 5)
         cnn_dropout=0.5
@@ -32,13 +34,18 @@ class DialogueGCN(nn.Module):
         self.wp = args.wp
         self.wf = args.wf
         self.device = args.device
-        self.cnn_feat_extractor = CNNFeatureExtractor(voca_size, e_dim, u_dim, cnn_filters,cnn_kernel_sizes,cnn_dropout, args)
+        
+        glv_pretrained = pickle.load(open(args.pretrained_word_vectors, 'rb'))
+        self.vocab_size, self.e_dim = glv_pretrained['embedding'].shape
+        print (self.vocab_size, self.e_dim, glv_pretrained['embedding'].shape) 
+        self.cnn_feat_extractor = CNNFeatureExtractor(self.vocab_size, self.e_dim, u_dim, cnn_filters,cnn_kernel_sizes,cnn_dropout, args)
+        self.cnn_feat_extractor.init_pretrained_embeddings_from_numpy(glv_pretrained['embedding'])        
+        
         self.rnn = SeqContext(u_dim, g_dim, args)
         self.edge_att = EdgeAtt(g_dim, args)
         self.gcn = GCN(g_dim, h1_dim, h2_dim, args)
         self.clf = Classifier(g_dim + h2_dim, hc_dim, tag_size, args)
         glv_pretrained = pd.read_pickle(args.pretrained_word_vectors)
-        self.cnn_feat_extractor.init_pretrained_embeddings_from_numpy(glv_pretrained['embedding'])
         edge_type_to_idx = {}
         for j in range(args.n_speakers):
             for k in range(args.n_speakers):
